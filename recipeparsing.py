0c78b65f2ba79a0675de359a1eed675deb4ca6a5
Michelle Yuen <mjyuen@princeton.edu>
2020-04-30 23:34:51 -0400

    try deploy

--- recipeparsing.py
+++ recipeparsing.py
@@ -8,7 +8,7 @@ def parse(text):
       comment = ""
       name = ""
     
-    unitSet = ([ "pinch", "pinches" "dash", "dashes", "teaspoon", "teaspoons", "tsp", "tablespoon", "tablespoons", "tbsp", "can", "cans", "cup", "cups", "slices", "slice", "dollop", "dollops", "scoops", "scoops", "gram", "grams", "containers", "container", "packets", "packet", "slice", "slices", "liters", "liter", "pounds", "pound", "lbs", "bunch", "bunches", "lb", "sprigs", "sprig", "ounces", "oz", "ounce", "pints", "pint", "gallon", "gallons", "medium", "large", "small", "branch" ])
+    unitSet = ([ "pinch", "pinches" "dash", "dashes", "teaspoon", "teaspoons", "tsp", "tsp.", "tablespoon", "tablespoons", "tbsp", "tbsp.", "package", "can", "cans", "cup", "cups", "slices", "slice", "dollop", "dollops", "scoops", "scoops", "gram", "grams", "containers", "container", "packets", "packet", "slice", "slices", "liters", "liter", "pounds", "pound", "lbs", "bunch", "bunches", "lb", "sprigs", "sprig", "ounces", "oz", "ounce", "pints", "pint", "gallon", "gallons", "medium", "large", "small", "branch" ])
 
     #testInput = "1 pinch of salt"
     #testInput = "1 stick of butter"
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from database import app
+import os
+
+port  = int(os.environ.get("PORT", 5000))
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=os.environ.get("PORT", 5000))
